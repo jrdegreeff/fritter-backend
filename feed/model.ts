d5486201490f3068431d82555331db4b91718b96
Jeremiah DeGreeff <degreeff@mit.edu>
2022-10-23 11:46:00 -0400

    data models for all concepts

---
+++ feed/model.ts
@@ -0,0 +1,44 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+import type {User} from '../user/model';
+import type {PopulatedFreet} from '../freet/model';
+
+type ParameterizedFeed<U,S,I> = {
+    _id: Types.ObjectId;
+    owner: U;
+    name: string;
+    sources: Array<S>;
+    items: Array<I>;
+};
+
+export type Feed = ParameterizedFeed<Types.ObjectId,Types.ObjectId,Types.ObjectId>;
+export type PopulatedFeed = ParameterizedFeed<User,User,PopulatedFreet>;
+
+const FeedSchema = new Schema<Feed>({
+    // The owner of the feed
+    owner: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    // The name of the feed
+    name: {
+        type: Schema.Types.String,
+        required: true
+    },
+    // The sources of the feed
+    sources: [{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    }],
+    // The items in the feed
+    items: [{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Freet'
+    }]
+});
+
+const FeedModel = model<Feed>('Feed', FeedSchema);
+export default FeedModel;
---
+++ thread/model.ts
@@ -0,0 +1,38 @@
+import type {Types} from 'mongoose';
+import {Schema, model} from 'mongoose';
+import type {PopulatedFreet} from '../freet/model';
+
+type ParameterizedThread<I> = {
+    _id: Types.ObjectId;
+    freet: I;
+    lineage: Array<I>;
+    children: Array<I>;
+};
+
+export type Thread = ParameterizedThread<Types.ObjectId>;
+export type PopulatedThread = ParameterizedThread<PopulatedFreet>;
+
+const ThreadSchema = new Schema<Thread>({
+    // The freet
+    freet: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        unique: true,
+        ref: 'Freet'
+    },
+    // The lineage of the freet
+    lineage: [{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Freet'
+    }],
+    // The children of the freet
+    children: [{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Freet'
+    }]
+});
+
+const ThreadModel = model<Thread>('Thread', ThreadSchema);
+export default ThreadModel;
